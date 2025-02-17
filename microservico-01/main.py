46b5fb373adb789d78353b446ec16d1323d8d0b2
euluis-dev <fluisf00@gmail.com>
2025-02-16 21:31:07 -0300

    add feature: feat: implement MongoDB connection and workout retrieval endpoint with error handling - Set up MongoDB connection using environment variable MONGODB_URI loaded from .env file - Created /workouts/{user_id} endpoint to fetch workouts for a specific user from the database - Convert user_id from string to ObjectId and handle conversion errors - Return a list of workouts with relevant details (nome, muscleGroups, date, description) - Added error messages for missing workouts or conversion issues with user_id

--- microservico-01/main.py
+++ microservico-01/main.py
@@ -4,14 +4,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 import base64 
+import os
+from dotenv import load_dotenv
+from bson import ObjectId
+
+load_dotenv()
 
 app = FastAPI() 
 
 # Conect MongoDB
-URI = 'MONGODB_URI'
+URI = os.getenv("MONGODB_URI")
 client = MongoClient(URI)
 db = client["BestFitData"]
 workouts_collection = db["workouts"]
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+print("MongoDB URI:", URI)
+
+
+@app.get("/workouts/{user_id}")
+async def get_workouts(user_id: str):
+    # Converte o user_id para ObjectId
+    try:
+        user_id_object = ObjectId(user_id)
+    except Exception as e:
+        return {"message": f"Erro ao converter user_id: {e}"}
+    
+    workouts = list(workouts_collection.find({"userId": user_id_object}))
+    
+    if not workouts:
+        return {"message": "Nenhum treino encontrado para este usuário."}
+    
+    workouts_clean = [
+        {"nome": workout["nome"], "muscleGroups": workout["muscleGroups"], "date": workout["date"], "description": workout["description"]}
+        for workout in workouts
+    ]
+    
+    return {"workouts": workouts_clean}
 
 
